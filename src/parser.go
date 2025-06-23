e931431ebc2bab2b84d9347013fc59d17beda491
Iqbal <imaniatfan@gmail.com>
2025-06-23 19:57:40 +0700

    temp fix: twitter fallthrough

--- src/parser.go
+++ src/parser.go
@@ -9,8 +9,9 @@ import (
 )
 
 type Post struct {
-	PostUrl   string `json:"post_url"`
-	ShoudlFix bool   `json:"should_url"`
+	PostUrl       string `json:"post_url"`
+	ShoudlFix     bool   `json:"should_url"`
+	SkipNextCheck bool   `json:"skip_next_check"`
 }
 
 var (
@@ -18,15 +19,17 @@ var (
 )
 
 func ParseUrl(discord *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.GuildID == viper.GetString("SKIP_SERVER") {
-		fmt.Println("skip")
-		return
+	if message.GuildID != viper.GetString("SKIP_SERVER") {
+		post = isTwitterUrl(message.Content)
 	}
 
-	post = isTwitterUrl(message.Content)
-	post = isInstaUrl(message.Content)
+	if post != nil && !post.SkipNextCheck {
+		post = isInstaUrl(message.Content)
+	}
 
+	fmt.Println(message.Member.User.Username)
 	fmt.Println(message.Content)
+	fmt.Println(" ")
 
 	if post.ShoudlFix {
 		_, err := discord.ChannelMessageSendReply(message.ChannelID, post.PostUrl, message.Reference())
@@ -41,7 +44,7 @@ func ParseUrl(discord *discordgo.Session, message *discordgo.MessageCreate) {
 		}
 	}
 
-	fmt.Println(post.PostUrl)
+	// fmt.Println(post.PostUrl)
 }
 
 func isTwitterUrl(url string) *Post {
@@ -54,6 +57,12 @@ func isTwitterUrl(url string) *Post {
 		post.ShoudlFix = true
 	case strings.Contains(url, "https://twitter.com"):
 		post.PostUrl = strings.Replace(url, "https://twitter.com", "https://fxtwitter.com", 1)
+		post.ShoudlFix = true
+	case strings.Contains(url, "https://www.x.com"):
+		post.PostUrl = strings.Replace(url, "https://www.x.com", "https://fxtwitter.com", 1)
+		post.ShoudlFix = true
+	case strings.Contains(url, "https://www.twitter.com"):
+		post.PostUrl = strings.Replace(url, "https://www.twitter.com", "https://fxtwitter.com", 1)
 		post.ShoudlFix = true
 	default:
 		post.PostUrl = url
@@ -69,10 +78,10 @@ func isInstaUrl(url string) *Post {
 	// TODO: regex
 	switch {
 	case strings.Contains(url, "https://instagram.com/p"):
-		post.PostUrl = strings.Replace(url, "https://instagram.com/p", "https://kkinstagram.com/p", 1)
+		post.PostUrl = strings.Replace(url, "https://instagram.com/p", "https://ddinstagram.com/p", 1)
 		post.ShoudlFix = true
 	case strings.Contains(url, "https://www.instagram.com/p"):
-		post.PostUrl = strings.Replace(url, "https://www.instagram.com/p", "https://kkinstagram.com/p", 1)
+		post.PostUrl = strings.Replace(url, "https://www.instagram.com/p", "https://ddinstagram.com/p", 1)
 		post.ShoudlFix = true
 	case strings.Contains(url, "https://instagram.com/reel"):
 		post.PostUrl = strings.Replace(url, "https://instagram.com/reel", "https://kkinstagram.com/reel", 1)
